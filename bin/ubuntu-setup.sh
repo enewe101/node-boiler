2c5a440b0a3d96f099a65750038b443ebec3cad1
Edward Newell <edward.newell@gmail.com>
2017-08-03 20:00:45 -0400

    fix bad url for getting env in ubuntu-setup

--- bin/ubuntu-setup.sh
+++ bin/ubuntu-setup.sh
@@ -1,5 +1,5 @@
 # Set environment
-source <(curl https://raw.githubusercontent.com/enewe101/webapp-boiler/master/.env)
+source <(curl https://raw.githubusercontent.com/enewe101/webapp-boiler/master/.env.dev)
 
 # Enable firewall
 ufw default deny incoming
